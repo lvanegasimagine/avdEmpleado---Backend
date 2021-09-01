8cb9e575e424464be1162af2175f8c95661f0108
Luis Vanegas <lvanegas1429@gmail.com>
2021-09-01 16:15:36 -0600

    Controlador updateEmpleado funcional

--- src/controller/empleado.controller.js
+++ src/controller/empleado.controller.js
@@ -29,7 +29,7 @@ const getByIdEmpleado = (async (req , res)=>{
     try {
         // Valida si el id es es el correcto
         if(!mongoose.isValidObjectId(req.params.id)){
-            return res.status(400).send('Invalid Product Id');
+            return res.status(400).send('Id Invalido');
         }
 
         const empleadoOne = await Empleado.findById(req.params.id);
@@ -97,18 +97,49 @@ const addEmpleado = (async (req , res)=>{
     }
 });
 
-const updateEmpleado = (req , res)=>{
+const updateEmpleado =(async (req , res)=>{
+
     try {
+        // Primero valida si el id es el correcto.
+        if(!mongoose.isValidObjectId(req.params.id)){
+            return res.status(400).send('Id Invalido');
+        }
+
+        const empleadoAc = await Empleado.findByIdAndUpdate(
+            req.params.id,
+            {
+                nombre: req.body.nombre,
+                direccion: req.body.direccion,
+                fechaNacimiento: req.body.fechaNacimiento,
+                inss: req.body.inss,
+                estadoCivil: req.body.estadoCivil,
+                celular: req.body.celular,
+                cedula: req.body.cedula,
+                email: req.body.email,
+                contacto: req.body.contacto,
+                area: req.body.area,
+                estudios: req.body.estudios,
+                fechaInicio: req.body.fechaInicio,
+                hijos: req.body.hijos,
+                laboral: req.body.laboral,
+                referencias: req.body.referencias,
+                usuario: req.body.usuario,
+            },
+            {new: true}
+        )
+
         res.status(200).json({
-           id: req.params.id
+           status: true,
+           data: empleadoAc
         })
         
     } catch (error) {
        res.status(200).json({
-         error: error
+           status: false,
+           error: error
       })
     }
-}
+});
 
 const deleteEmpleado = (req , res)=>{
     try {
