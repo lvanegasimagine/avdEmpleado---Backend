747d81e3ca6f03c657fad9dc81558ce122260b47
Luis Vanegas <lvanegas1429@gmail.com>
2021-09-01 15:16:18 -0600

    getByIdEmpleado funcional

--- src/controller/empleado.controller.js
+++ src/controller/empleado.controller.js
@@ -1,4 +1,5 @@
 const Empleado = require('../models/Empleado');
+const mongoose = require('mongoose');
 
 
 const getEmpleado = (async (req, res) => {
@@ -23,21 +24,39 @@ const getEmpleado = (async (req, res) => {
     }
 });
 
-const getByIdEmpleado = (req , res)=>{
-   try {
-       res.status(200).json({
-          id: req.params.id
-       })
+const getByIdEmpleado = (async (req , res)=>{
+
+    try {
+        // Valida si el id es es el correcto
+        if(!mongoose.isValidObjectId(req.params.id)){
+            return res.status(400).send('Invalid Product Id');
+        }
+
+        const empleadoOne = await Empleado.findById(req.params.id);
+        
+        if(!empleadoOne){
+            res.status(500).json({
+                error: err,
+                success: false
+            })
+        }
+        res.send(empleadoOne);
     
-   } catch (error) {
-      res.status(200).json({
-        error: error
-     })
-   }
-}
+
+        
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            error: error
+        });
+    }
+});
 
 const addEmpleado = (async (req , res)=>{
+    
+    const existeEmail = await Empleado.findOne({email: req.body.email});
 
+    if(existeEmail) return res.status(400).json({status: true, message: 'Email ya registrado'});
     try {
         const empleado = new Empleado({
             nombre: req.body.nombre,
