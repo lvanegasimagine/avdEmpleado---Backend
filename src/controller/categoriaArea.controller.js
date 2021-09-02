4d9e65ad3666d5ca5279fa03b4e9d67052eee647
Luis Vanegas <lvanegas1429@gmail.com>
2021-09-02 15:04:13 -0600

    Creando getCategoriaArea funcional

--- src/controller/categoriaArea.controller.js
+++ src/controller/categoriaArea.controller.js
@@ -1,6 +1,26 @@
-const getCategoriaArea = (req,res) => {
-    return res.send('Hola perro')
-}
+const mongoose = require('mongoose');
+const CategoriaArea = require('../models/CategoriaArea');
+
+const getCategoriaArea = (async (req,res) => {
+    try {
+        const categoriaAreaList = await CategoriaArea.find();
+
+        if(!categoriaAreaList){
+            res.status(500).json({status:false, message: "Listado de categoria vacio"})
+        }
+
+        res.status(200).json({
+            status: true,
+            data: categoriaAreaList
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            status:false, 
+            error: error
+        });
+    }
+});
 
 
 module.exports = {
