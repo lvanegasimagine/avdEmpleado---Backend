664873d3453535c81211ff4ad8b345fa6172e176
Luis Vanegas <lvanegas1429@gmail.com>
2021-09-17 12:46:04 -0600

    Verificacion empleado get

--- src/routes/empleado.routes.js
+++ src/routes/empleado.routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const { getEmpleado, getByIdEmpleado, addEmpleado, updateEmpleado, deleteEmpleado } = require('../controller/empleado.controller');
+const verifyToken = require('../middleware/validarToken');
 
-router.get('/', getEmpleado)
+router.get('/', verifyToken, getEmpleado)
 router.get('/:id', getByIdEmpleado)
 router.post('/', addEmpleado)
 router.put('/:id', updateEmpleado)
